3bd38eb45756057837d07b9ebb354f0b2718d97e
Vineesh Vijayan <vineeshrk@gmail.com>
2020-04-12 09:02:13 +0530

    Add files via upload

--- jobs/settings.py
+++ jobs/settings.py
@@ -58,11 +58,11 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.sqlite3',
-#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     # },
+ DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      },
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'ghuyhfun',
@@ -71,18 +71,18 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 #         'HOST': 'baasu.db.elephantsql.com',
 #         'PORT': '5432',
 #     }
-#}
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'iDIL',
-        'USER': 'postgres',
-        'PASSWORD': 'manager@123',
-        'HOST': 'localhost',
-        'PORT': '5430',
-    }
-
 }
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'iDIL',
+#        'USER': 'postgres',
+#        'PASSWORD': 'manager@123',
+#        'HOST': 'localhost',
+#        'PORT': '5430',
+#    }
+#
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
