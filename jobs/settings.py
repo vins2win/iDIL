3d4a4d8fd51bd895eed51c425be2e2685b05e42e
Vineesh Vijayan <vineeshrk@gmail.com>
2020-04-12 09:05:11 +0530

    Add files via upload

--- jobs/settings.py
+++ jobs/settings.py
@@ -58,11 +58,12 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
- DATABASES = {
-      'default': {
-          'ENGINE': 'django.db.backends.sqlite3',
-          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-      },
+DATABASES = {
+  'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  },
+}
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'ghuyhfun',
@@ -71,7 +72,7 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 #         'HOST': 'baasu.db.elephantsql.com',
 #         'PORT': '5432',
 #     }
-}
+
 #DATABASES = {
 #   'default': {
 #       'ENGINE': 'django.db.backends.postgresql',
